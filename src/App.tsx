966278d208290b2a05f61454d694bc1feef92703
dlevenson44 <daniellevenson4@gmail.com>
2023-12-18 16:10:08 -0500

    add img scaling support

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,15 @@ interface AppState {
 	imgUrl: string
 	error: boolean
 	rotation: number
+	imgScale: number
 }
 
 const App: React.FC = () => {
 	const [state, setState] = React.useState<AppState>({
 		imgUrl: '',
 		error: false,
-		rotation: 0
+		rotation: 0,
+		imgScale: 100
 	})
 
 	React.useEffect(() => {
@@ -26,16 +28,27 @@ const App: React.FC = () => {
 		}
 	}, [state.imgUrl])
 
-	const handleChange = (e: React.FormEvent<HTMLInputElement>) =>
+	const handleUrlChange = (e: React.FormEvent<HTMLInputElement>) =>
 		setState({
 			...state,
 			imgUrl: e.currentTarget.value,
 		})
 
+	const handleScaleChange = (e: React.FormEvent<HTMLInputElement>) => {
+		const castValue = Number(e.currentTarget.value)
+		// Keep image scaling between 0 and 200%
+		if ((castValue <= 200 && castValue >= 0)) {
+			setState({
+				...state,
+				imgScale: castValue,
+			})
+		}
+	}
+
 	return (
 		<div className="App">
 			<TopContent
-				onChange={(e) => handleChange(e)}
+				onChange={(e) => handleUrlChange(e)}
 				imgUrl={state.imgUrl}
 				error={state.error}
 			/>
@@ -43,18 +56,24 @@ const App: React.FC = () => {
 				<ImageActionControls
 					onLeftRotateClick={() => setState({ ...state, rotation: state.rotation - 90 })}
 					onRightRotateClick={() => setState({ ...state, rotation: state.rotation + 90 })}
+					onScaleChange={(e) => handleScaleChange(e)}
+					imgScale={state.imgScale}
 				/>
 				{!state.error && !!state.imgUrl.length && (
 					<div>
 						{/* <ImageActionControls /> */}
-						<img
-							className="url-image"
-							style={{ transform: `rotate(${state.rotation}deg)` }}
-							alt="Broken Image :("
-							height={350}
-							width={350}
-							src={state.imgUrl}
-						/>
+						<div className="img-container">
+							<img
+								className="url-image"
+								style={{
+									transform: `rotate(${state.rotation}deg)`,
+									maxHeight: `${state.imgScale}%`,
+									maxWidth: `${state.imgScale}%`
+								}}
+								alt="Broken Image :("
+								src={state.imgUrl}
+							/>
+						</div>
 					</div>
 				)}
 			</div>
--- src/components/image-action-controls/ImageActionControls.tsx
+++ src/components/image-action-controls/ImageActionControls.tsx
@@ -5,16 +5,28 @@ import { MdOutlineRotateLeft, MdOutlineRotateRight } from 'react-icons/md'
 interface ImageActionControlsProps {
   onLeftRotateClick: () => void
   onRightRotateClick: () => void
+  onScaleChange: (e: React.FormEvent<HTMLInputElement>) => void
+  imgScale: number
 }
 
 const ImageActionControls: React.FC<ImageActionControlsProps> = ({
 	onLeftRotateClick,
-	onRightRotateClick
+	onRightRotateClick,
+	onScaleChange,
+	imgScale,
 }) => {
 	return (
 		<div>
 			<button title="Rotate Left" onClick={onLeftRotateClick}><MdOutlineRotateLeft /></button>
 			<button title="Rotate Right" onClick={onRightRotateClick}><MdOutlineRotateRight /></button>
+			<input
+				className="scale-img-input"
+				type="number"
+				placeholder="Enter Image URL To Get Started!"
+				onChange={onScaleChange}
+				value={imgScale}
+			/>
+			<span>%</span>
 		</div>
 	)
 }
